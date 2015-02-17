57aaa42d46fd3016c4e343a8e74b248a5bbac08a
TJ <TJ.TJ.com>
2015-02-16 19:43:20 -0500

    Beautify javascript files.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,26 @@
 app = {
-		init :  function() {
-				log.info("app log started");
-		},
-         start : function(args){
-			    log.info("start method");
-				return ("start method");
-		 },		  
-		 end : function(args){
-			    log.info("end method");
-				return ("end method");
-		 },	
-		 assert : function (condition, message){
-			 try{
-				if (!eval(condition[0])) {
-					message = message || "Assertion failed";
-					if (typeof Error !== "undefined") {
-						throw new Error(message);
-					}
-				throw message; // Fallback
-				}
-			 }
-			catch (err){
-				return ("error: " + err);
-			} 
-		}
+    init: function() {
+        log.info("app log started");
+    },
+    start: function(args) {
+        log.info("start method");
+        return ("start method");
+    },
+    end: function(args) {
+        log.info("end method");
+        return ("end method");
+    },
+    assert: function(condition, message) {
+        try {
+            if (!eval(condition[0])) {
+                message = message || "Assertion failed";
+                if (typeof Error !== "undefined") {
+                    throw new Error(message);
+                }
+                throw message; // Fallback
+            }
+        } catch (err) {
+            return ("error: " + err);
+        }
+    }
 }
